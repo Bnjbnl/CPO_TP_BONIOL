d1e3f75153a4cee13239b4123726545eef619653
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-10-25 16:34:43 +0200

    tp 2 question 11

--- src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
+++ src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
@@ -12,12 +12,16 @@ package com.mycompany.tp2_bieres_boniol;
 public class TP2_Bieres_BONIOL {
 
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0 ;
-        uneBiere.brasserie = "Dubuisson" ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
         uneBiere.ouverture = false ;
         uneBiere.lireEtiquette();
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe");
+        autreBiere.lireEtiquette();
+        BouteilleBiere premiereBiere = new BouteilleBiere("1664", 5.5 ,"Le Canon") ;
+        premiereBiere.lireEtiquette();
+        BouteilleBiere secondeBiere = new BouteilleBiere("Pelforth", 5.8 ,"Brasserie du Pélican") ;
+        secondeBiere.lireEtiquette();
+            BouteilleBiere troisiemeBiere = new BouteilleBiere("8.6", 7.9 ,"Royal Swinkels") ;
+        troisiemeBiere.lireEtiquette();
     }
 }
