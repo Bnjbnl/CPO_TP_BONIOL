4888aecca5a6aeaa6760efbcc846bb78c7302a26
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-10-29 11:01:44 +0200

    -Création du projet -Ajout des attributs de la bière -Ajout d'une méthode d'affichage -Création d'un objet uneBiere, affections de valeurs et affichage -Création d'une autreBiere de manière analogue -Création des objets d'une autre manière -Création d'une méthode Décapsuler() -Ajout d'une autre méthode d'affichage -Ajout de commentaires

--- src/main/java/com/mycompany/tp2_bieres_boniol/BouteilleBiere.java
+++ src/main/java/com/mycompany/tp2_bieres_boniol/BouteilleBiere.java
@@ -9,16 +9,25 @@ package com.mycompany.tp2_bieres_boniol;
  * @author Benjamin
  */
 public class BouteilleBiere {
-
+//création d'une classe BouteilleBiere
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
 
+    /**
+     *Création d'une méthode pour lire les valeurs de l'objet
+     */
     public void lireEtiquette() {
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
     }
-
+    
+    /**
+     * Création d'une méthode pour definir les valeurs de l'objet
+     * @param unNom représente le nom de la bière
+     * @param unDegre représente le degré d'alcool de la bière
+     * @param uneBrasserie représente la brasserie de la bière
+     */
     public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
         nom = unNom;
         degreAlcool = unDegre;
@@ -26,6 +35,10 @@ public class BouteilleBiere {
         ouverte = false;
     }
 
+    /**
+     *Création d'une méthode pour ouvrir une bière fermée
+     * @return si la bière à été ouverte ou non sous forme de booleen
+     */
     public boolean Décapsuler() {
         if (ouverte == false) {
             ouverte = true;
@@ -36,6 +49,10 @@ public class BouteilleBiere {
         }
     }
 
+    /**
+     *Création d'une autre méthode d'affichage
+     * @return une chaine qui va être affiché
+     */
     @Override
     public String toString() {
         String chaine_a_retourner;
--- src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
+++ src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
@@ -11,22 +11,27 @@ package com.mycompany.tp2_bieres_boniol;
 public class TP2_Bieres_BONIOL {
 
     public static void main(String[] args) {
-        //BouteilleBiere uneBiere = new BouteilleBiere() ;
+        //BouteilleBiere uneBiere = new BouteilleBiere() ; 
+        //Création d'un objet uneBiere
         //uneBiere.nom = "Cuvée des trolls";
         //uneBiere.degreAlcool = 7.0 ;
+        //attribution de valeurs
         //uneBiere.lireEtiquette();
         //BouteilleBiere autreBiere = new BouteilleBiere();
         //autreBiere.nom = "Leffe";
         //autreBiere.degreAlcool = 6.6;
         //autreBiere.brasserie="Abbaye de Leffe";
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+        //Création d'un objet uneBière et attribution de valeurs a ce dernier
         BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         BouteilleBiere premiereBiere = new BouteilleBiere("1664", 5.5, "Le Canon");
         premiereBiere.lireEtiquette();
+        //Permet d'afficher les valeurs de premiereBiere
         BouteilleBiere secondeBiere = new BouteilleBiere("Pelforth", 5.8, "Brasserie du Pélican");
         secondeBiere.lireEtiquette();
         BouteilleBiere troisiemeBiere = new BouteilleBiere("8.6", 7.9, "Royal Swinkels");
         troisiemeBiere.lireEtiquette();
         System.out.println(autreBiere);
+        //Autre méthode pour afficher autreBiere
     }
 }
