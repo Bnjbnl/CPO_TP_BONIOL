472120ea260129a0aad925e298ca97ba8d828230
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-10-29 10:29:51 +0200

    -Création du projet -Ajout des attributs de la bière

---
+++ src/main/java/com/mycompany/tp2_bieres_boniol/BouteilleBiere.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_bieres_boniol;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class BouteilleBiere {
+    String nom;
+    float degreeAlcool;
+    String brasserie;
+    boolean ouverte;
+    
+}
