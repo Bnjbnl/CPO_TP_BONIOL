0b7582e19df50e8014b14e1e40a66e8d9959b4bf
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-10-29 10:52:50 +0200

    -Création du projet -Ajout des attributs de la bière -Ajout d'une méthode d'affichage -Création d'un objet uneBiere, affections de valeurs et affichage -Création d'une autreBiere de manière analogue -Création des objets d'une autre manière -Création d'une méthode Décapsuler() -Ajout d'une autre méthode d'affichage

--- src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
+++ src/main/java/com/mycompany/tp2_bieres_boniol/TP2_Bieres_BONIOL.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.tp2_bieres_boniol;
 
 /**
@@ -20,7 +19,14 @@ public class TP2_Bieres_BONIOL {
         //autreBiere.nom = "Leffe";
         //autreBiere.degreAlcool = 6.6;
         //autreBiere.brasserie="Abbaye de Leffe";
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson") ;
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        BouteilleBiere premiereBiere = new BouteilleBiere("1664", 5.5, "Le Canon");
+        premiereBiere.lireEtiquette();
+        BouteilleBiere secondeBiere = new BouteilleBiere("Pelforth", 5.8, "Brasserie du Pélican");
+        secondeBiere.lireEtiquette();
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("8.6", 7.9, "Royal Swinkels");
+        troisiemeBiere.lireEtiquette();
+        System.out.println(autreBiere);
     }
 }
